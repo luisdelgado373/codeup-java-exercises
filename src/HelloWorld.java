dd1f6ce14886e6f4339bc494becf744ad2e76839
Luis Delgado <delgadoluis0510@gmail.com>
2019-09-17 09:12:36 -0500

    updates

---
+++ src/HelloWorld.java
@@ -0,0 +1,39 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.print("Hello, ");
+//        System.out.print("World! ");
+//        int myFavoriteNumber = 23;
+//        String myString = "This is a string";
+//        float myNumber = 3.14F;
+//        System.out.println(myFavoriteNumber);
+//        System.out.println(myString);
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        difference is one is pre increment and other is post.
+//        system will not allow you as "class" is reserved
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        code will not return an output. runtime error
+//        int three = (int) "three";
+//        system will tell you there is an error before having to run the code. compiling error
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+    }
+}
