d16fb857d58dfdb55f6e04918440c8aa5c3fa221
Luis Delgado <delgadoluis0510@gmail.com>
2019-09-19 13:50:36 -0500

    methods exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 public class MethodsExercises {
     public static void main(String[] args) {
-//        int added = addition(2, 4);
-//        int subtracted = subtraction(2, 4);
-//        int multiplied = multiplication(2, 4);
-//        int divided = division(2, 4);
-//        int modulused = modulus(10, 6);
+//        int added = addition(1, 2);
+//        int subtracted = subtraction(3, 4);
+//        int multiplied = multiplication(5, 6);
+//        int divided = division(8, 4);
+//        int modulused = modulus(10, 7);
+//        int userInput = getInteger(1, 10);
+//        int factorialed = factorial(1, 10);
+
 //        System.out.printf("Here are your numbers added: %d\n", added);
 //        System.out.printf("Here are your numbers subtracted: %d\n", subtracted);
 //        System.out.printf("Here are your numbers multiplied: %d\n", multiplied);
@@ -26,7 +30,42 @@ public class MethodsExercises {
 //    public static int modulus(int num1, int num2) {
 //        return num1 % num2;
 //    }
-    public static int getInteger(int min, int max) {
 
-    }
+//    public static int getInteger(int min, int max) {
+//        System.out.print("Enter a number between 1 and 10: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int input = scanner.nextInt();
+//        if(input>min && input<=max){
+//            System.out.println("That is a valid input");
+//            return input;
+//        } else {
+//            System.out.println("That is an invalid input");
+//            return getInteger(min,max);
+//        }
+//    }
+
+//    public static int factorial(int min, int max) {
+//        System.out.print("Enter a number between 1 and 10: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int input = scanner.nextInt();
+//        if(input>min && input<=max){
+//            System.out.println("That is a valid input");
+//                int answer = 1;
+//            for(int i=1;i<=input;i++){
+//                answer *= i;
+//            }
+//                System.out.println(answer);
+//        System.out.print("Would you like to continue? Y/N: ");
+//        Scanner userReply = new Scanner(System.in);
+//        String reply = userReply.nextLine();
+//        if(reply.equals("y")){
+//            return factorial(min, max);
+//        } else {
+//            return 0;
+//        }
+//        } else {
+//            System.out.println("That is an invalid input");
+//            return factorial(min,max);
+//        }
+//    }
 }
